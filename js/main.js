df0b6c0e8d508ad702c15915b0724da8bdc5c7bd
Erika Moller <erikamoller@gmail.com>
2015-01-19 20:53:50 -0600

    Finished Questions 1, 5-8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,84 @@
 //Question #1:
 
-var random = function(placeholder) {
-	var computerChoice = Math.floor(Math.random() * placeholder.length);
-	return placeholder[computerChoice];
-}
+// var random = function(placeholder) {
+// 	var computerChoice = Math.floor(Math.random() * placeholder.length);
+// 	return placeholder[computerChoice];
+// }
+
+
+//Question #5:
+
+
+// function students(placeholder) {
+// 	if (typeof placeholder === "object") {
+// 		placeholder;
+// 	} else {
+// 		throw "There are no strings in the array";
+// 	}
+
+// 	var computerChoice1 = Math.floor(Math.random() * placeholder.length);
+// 	console.log(computerChoice1);
+
+// 	var computerChoice2 = Math.floor(Math.random() * placeholder.length);
+// 	console.log(computerChoice2);
+
+// 		while(computerChoice1 === computerChoice2) {
+// 			computerChoice1++;
+// 		}
+
+// 	var myNewArray1 = placeholder[computerChoice1];
+// 	var myNewArray2 = placeholder[computerChoice2];
+	
+// 	return myNewArray1 + ", " + myNewArray2
+
+// }
+
+// Question #6:
+
+// function dashing(placeholder) {
+// 	if (typeof placeholder === "string") {
+// 		placeholder;
+// 	} else {
+// 		throw "No strings were taken";
+// 	}
+// 	var stringSplit = (placeholder.split(" "));
+
+// 	var stringJoin = (stringSplit.join('-'));
+// 	return stringJoin
+// }
+
+// Question #7:
+
+// function alternateString(placeholder) {
+// 	if (typeof placeholder === "string") {
+// 		placeholder;
+// 	} else {
+// 		throw "No strings were taken";
+// 	}
+
+// 	placeholder = placeholder.replace(/ /g, "-");
+// 	return placeholder
+// }
+
+// Question #8:
+
+// function twostrings(string1, string2) {
+// 	if (typeof string1 === "string") {
+// 		string1;
+// 	} else {
+// 		throw "First parameter is not a string";
+// 	}
+// 	if (typeof string2 === "string") {
+// 		string2;
+// 	} else {
+// 		throw "Second parameter is not a string";
+// 	}
+
+// 	var finalString = string1 +" "+string2;
+// 	finalString = finalString.replace(/ /g, "-");
+// 	console.log(finalString);
+// }
+
+// Question #9:
+
+// Question #10:
